a1bbe59270a0d64a429d806d278304358d570bdb
丁真 <327112257@qq.com>
2023-12-02 20:43:34 +0800

    7.39 keybind bugfix

--- src/main/java/net/wurstclient/options/KeybindEditorScreen.java
+++ src/main/java/net/wurstclient/options/KeybindEditorScreen.java
@@ -28,17 +28,17 @@ public final class KeybindEditorScreen extends Screen
 	
 	public KeybindEditorScreen(Screen prevScreen)
 	{
-		super(Text.literal("disconnect.nochatreports.server"));
+		super(Text.literal(""));
 		this.prevScreen = prevScreen;
 		
-		key = "You do not have No Chat Reports, and this server is configured to require it on client!";
+		key = "NONE";
 		oldKey = null;
 		oldCommands = null;
 	}
 	
 	public KeybindEditorScreen(Screen prevScreen, String key, String commands)
 	{
-		super(Text.literal("disconnect.nochatreports.server"));
+		super(Text.literal(""));
 		this.prevScreen = prevScreen;
 		
 		this.key = key;
@@ -50,19 +50,19 @@ public final class KeybindEditorScreen extends Screen
 	public void init()
 	{
 		addDrawableChild(ButtonWidget
-			.builder(Text.literal("gui.wurst.nochatreports.ncr_mod_server.message"),
+			.builder(Text.literal("Change Key"),
 				b -> client.setScreen(new PressAKeyScreen(this)))
 			.dimensions(width / 2 - 100, 60, 200, 20).build());
 		
-		addDrawableChild(ButtonWidget.builder(Text.literal("button.wurst.nochatreports.signatures_status"), b -> save())
+		addDrawableChild(ButtonWidget.builder(Text.literal("Save"), b -> save())
 			.dimensions(width / 2 - 100, height / 4 + 72, 200, 20).build());
 		
 		addDrawableChild(ButtonWidget
-			.builder(Text.literal("VanillaSpoof: "), b -> client.setScreen(prevScreen))
+			.builder(Text.literal("Cancel"), b -> client.setScreen(prevScreen))
 			.dimensions(width / 2 - 100, height / 4 + 96, 200, 20).build());
 		
 		commandField = new TextFieldWidget(textRenderer, width / 2 - 100, 100,
-			200, 20, Text.literal("disconnect.nochatreports.server"));
+			200, 20, Text.literal(""));
 		commandField.setMaxLength(65536);
 		addSelectableChild(commandField);
 		setFocused(commandField);
@@ -95,13 +95,13 @@ public final class KeybindEditorScreen extends Screen
 		renderBackground(context, mouseX, mouseY, partialTicks);
 		
 		context.drawCenteredTextWithShadow(textRenderer,
-			(oldKey != null ? "on" : "off") + "gui.wurst.generic.allcaps_", width / 2, 20,
+			(oldKey != null ? "Edit" : "Add") + " Keybind", width / 2, 20,
 			0xffffff);
 		
 		context.drawTextWithShadow(textRenderer,
-			"blocked" + key.replace("allowed", "disconnect.nochatreports.server"), width / 2 - 100, 47,
+			"Key: " + key.replace("key.keyboard.", ""), width / 2 - 100, 47,
 			0xa0a0a0);
-		context.drawTextWithShadow(textRenderer, "gui.toMenu",
+		context.drawTextWithShadow(textRenderer, "Commands (separated by ';')",
 			width / 2 - 100, 87, 0xa0a0a0);
 		
 		commandField.render(context, mouseX, mouseY, partialTicks);
